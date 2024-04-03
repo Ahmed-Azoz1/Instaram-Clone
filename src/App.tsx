6611ee367c7460873a0845559cd3e446aa718d3e
Ahmed-Azoz1 <ahmedhjg123@gmail.com>
2024-04-03 20:18:24 +0200

    All pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import { Routes,Route } from 'react-router-dom'
 import SigninForm from './_auth/forms/SigninForm'
 import SignupForm from './_auth/forms/SignupForm'
-import { Home } from './_root/pages'
+import { Home, LikedPosts,Explore,Saved,AllUsers,CreatePost,EditPost,PostDetails,Profile,UpdateProfile } from './_root/pages'
 import AuthLayout from './_auth/AuthLayout'
 import RootLayout from './_root/RootLayout'
 import { Toaster } from './components/ui/toaster'
 import './globals.css'
+
 
 const App = () => {
     return (
@@ -21,6 +22,15 @@ const App = () => {
                 {/* Private Routes */}
                 <Route element={<RootLayout />}>
                     <Route index  element={<Home />}/>
+                    <Route path='/explore'  element={<Explore />}/>
+                    <Route path='/saved'  element={<Saved />}/>
+                    <Route path='/all-users'  element={<AllUsers />}/>
+                    <Route path='/create-post'  element={<CreatePost />}/>
+                    <Route path='/update-post/:id'  element={<EditPost />}/>
+                    <Route path='/posts/:id'  element={<PostDetails />}/>
+                    <Route path='/profile/:id/*'  element={<Profile />}/>
+                    <Route path='/update-profile/:id'  element={<UpdateProfile />}/>
+                    <Route path='/liked-posts/:id'  element={<LikedPosts />}/>
                 </Route>
             </Routes>
             <Toaster />

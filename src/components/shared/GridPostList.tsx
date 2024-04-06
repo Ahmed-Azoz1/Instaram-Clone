80c181727f31a1c14c496ff73132b21d741ebbb4
Ahmed-Azoz1 <ahmedhjg123@gmail.com>
2024-04-06 18:02:31 +0200

    GridPostList Page

--- src/components/shared/GridPostList.tsx
+++ src/components/shared/GridPostList.tsx
@@ -1,8 +1,48 @@
+import { useUserContext } from "@/context/AuthContext"
+import { Models } from "appwrite"
+import { Link } from "react-router-dom"
+import PostStats from "./PostStats";
 
 
-const GridPostList = () => {
+type GridPostListProps = {
+    posts:Models.Document[];
+    showUser?:boolean;
+    showStats?:boolean;
+}
+
+const GridPostList = ({posts,showUser=true,showStats=true}:GridPostListProps) => {
+
+    const {user} = useUserContext();
+
+
     return (
-        <div>GridPostList</div>
+        <ul className="grid-container">
+            {
+                posts.map((post,index)=>(
+                    <li className="relative min-w-80 h-80" key={index}>
+                        <Link to={`/posts/${post.$id}`} className="grid-post_link">
+                            <img className="h-full w-full object-cover" src={post.imageUrl} alt="post" />
+                        </Link>
+
+                        <div className="grid-post_user">
+                            {
+                                showUser && 
+                                (
+                                    <div className="flex flex-1 items-center justify-start gap-2">
+                                        <img className="h-8 w-8 rounded-full" src={post.creator.imageUrl} alt="creator" />
+                                        <p className="line-clamp-1">{post?.creator?.name}</p>
+                                    </div>
+                                )
+                            }
+                            {
+                                showStats && <PostStats post={post} userId={user.id} />
+                            }
+                        </div>
+
+                    </li>
+                ))
+            }
+        </ul>
     )
 }
 

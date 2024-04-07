c9a54d7aea33a0ccadda0e95043308d9b1c6f5e7
Ahmed-Azoz1 <ahmedhjg123@gmail.com>
2024-04-07 16:17:38 +0200

    Profile Page

--- src/_root/pages/Explore.tsx
+++ src/_root/pages/Explore.tsx
@@ -1,20 +1,30 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import { Input } from "@/components/ui/input"
 import SearchResults from "@/components/shared/SearchResults";
 import GridPostList from "@/components/shared/GridPostList";
 import { useGetPosts, useSearchPosts } from "@/lib/react-query/queriesAndMutations";
 import useDebounce from "@/hooks/useDebounce";
 import Loader from "@/components/shared/Loader";
+import { useInView } from "react-intersection-observer";
+
+
+
 
 
 const Explore = () => {
 
     const {data:posts,fetchNextPage,hasNextPage} = useGetPosts();
 
+    const {ref,inView} = useInView();
+
     const [searchValue,setSearchValue] = useState('');
 
-    const debouncedValue = useDebounce(searchValue,500);
-    const {data:searchedPosts,isFetching:isSearchFetching} = useSearchPosts(searchValue);
+    const debouncedSearch = useDebounce(searchValue, 500);
+    const { data: searchedPosts, isFetching: isSearchFetching } = useSearchPosts(debouncedSearch);
+
+    useEffect(()=>{
+        if(inView && !searchValue) fetchNextPage()
+    },[inView,searchValue])
 
     if(!posts){
         return (
@@ -39,16 +49,16 @@ const Explore = () => {
                     <img src="/assets/icons/search.svg" alt="search" width={24} height={24} />
                     <Input onChange={(e)=>{setSearchValue(e.target.value)}} type="text" placeholder="Search" className="explore-search" value={searchValue} />
                 </div>
+            </div>
 
-                <div className="flex-between w-full max-w-5xl mt-16 mb-7">
-                    <h3 className="body-bold md:h3-bold">
-                        Popular Today
-                    </h3>
+            <div className="flex-between w-full max-w-5xl mt-16 mb-7">
+                <h3 className="body-bold md:h3-bold">
+                    Popular Today
+                </h3>
 
-                    <div className="flex-center gap-3 bg-dark-3 rounded-xl px-4 py-2 cursor-pointer">
-                        <p className="small-medium md:base-medium text-light-2">All</p>
-                        <img width={20} height={20} src="/assets/icons/filter.svg" alt="filter" />
-                    </div>
+                <div className="flex-center gap-3 bg-dark-3 rounded-xl px-4 py-2 cursor-pointer">
+                    <p className="small-medium md:base-medium text-light-2">All</p>
+                    <img width={20} height={20} src="/assets/icons/filter.svg" alt="filter" />
                 </div>
             </div>
 
@@ -74,8 +84,7 @@ const Explore = () => {
                     <div ref={ref} className="mt-10">
                         <Loader />
                     </div>
-                ) :
-                ()
+                )
             }
 
         </div>
--- src/lib/react-query/queriesAndMutations.ts
+++ src/lib/react-query/queriesAndMutations.ts
@@ -1,6 +1,6 @@
 import {useQuery,useMutation,useQueryClient,useInfiniteQuery} from '@tanstack/react-query'
-import { createUserAccount, deletePost, deleteSavedPost, getCurrentUser, getInfinitePosts, getPostById, getRecentPosts, likePost, savePost, searchPosts, signInAccount, signOutAccount, updatePost } from '../appwrite/api'
-import { INewPost, INewUser, IUpdatePost } from '@/types'
+import { createUserAccount, deletePost, deleteSavedPost, getCurrentUser, getInfinitePosts, getPostById, getRecentPosts, getUserById, getUserPosts, getUsers, likePost, savePost, searchPosts, signInAccount, signOutAccount, updatePost, updateUser } from '../appwrite/api'
+import { INewPost, INewUser, IUpdatePost, IUpdateUser } from '@/types'
 
 import { QUERY_KEYS } from './queryKeys'
 import { createPost } from '../appwrite/api'
@@ -193,3 +193,51 @@ export const useSearchPosts = (searchTerm:string)=>{
         enabled:!!searchTerm
     })
 }
+
+
+
+// ============================== Get User Posts ==============================
+export const useGetUserPosts = (userId?: string) => {
+    return useQuery({
+        queryKey: [QUERY_KEYS.GET_USER_POSTS, userId],
+        queryFn: () => getUserPosts(userId),
+        enabled: !!userId,
+    });
+};
+
+
+
+export const useGetUsers = (limit?: number) => {
+    return useQuery({
+        queryKey: [QUERY_KEYS.GET_USERS],
+        queryFn: () => getUsers(limit),
+    });
+};
+
+
+
+export const useGetUserById = (userId: string) => {
+    return useQuery({
+        queryKey: [QUERY_KEYS.GET_USER_BY_ID, userId],
+        queryFn: () => getUserById(userId),
+        enabled: !!userId,
+    });
+};
+
+
+
+
+export const useUpdateUser = () => {
+    const queryClient = useQueryClient();
+    return useMutation({
+        mutationFn: (user: IUpdateUser) => updateUser(user),
+        onSuccess: (data) => {
+            queryClient.invalidateQueries({
+            queryKey: [QUERY_KEYS.GET_CURRENT_USER],
+            });
+            queryClient.invalidateQueries({
+            queryKey: [QUERY_KEYS.GET_USER_BY_ID, data?.$id],
+            });
+        },
+    });
+};
